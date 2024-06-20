5b2b4af9bb314053bd5ea563a8bc2ca7bb19c2da
yuvrajthakur18 <ysthakur180401@gmail.com>
2024-06-21 01:28:35 +0530

    Added service and repository layers with interfaces and implementations

--- src/main/java/com/yuvraj/service/AdminServiceImplementation.java
+++ src/main/java/com/yuvraj/service/AdminServiceImplementation.java
@@ -1,5 +1,44 @@
 package com.yuvraj.service;
 
-public class AdminServiceImplementation {
+import com.yuvraj.model.Player;
+import com.yuvraj.model.Team;
 
+public class AdminServiceImplementation implements AdminService{
+
+	@Override
+	public Team createTeam(Team team) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Team updateTeam(Long id, Team team) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteTeam(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player createPlayer(Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player updatePlayer(Long id, Player player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletePlayer(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
--- src/main/java/com/yuvraj/service/OrganizerServiceImplementation.java
+++ src/main/java/com/yuvraj/service/OrganizerServiceImplementation.java
@@ -1,44 +1,5 @@
 package com.yuvraj.service;
 
-import com.yuvraj.model.Player;
-import com.yuvraj.model.Team;
+public class OrganizerServiceImplementation {
 
-public class OrganizerServiceImplementation implements AdminService{
-
-	@Override
-	public Team createTeam(Team team) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Team updateTeam(Long id, Team team) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteTeam(Long id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Player createPlayer(Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Player updatePlayer(Long id, Player player) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deletePlayer(Long id) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
